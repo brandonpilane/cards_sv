a94479a01bce337e3452c75937d31c0dbe632adb
Brandon <BrandonPilane@gmail.com>
2025-03-25 21:53:24 +0200

    fix: Handling of lightmode and darkmode styling for navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,42 @@
 </nav>
 
 {@render children()}
+
+<style>
+	nav {
+		margin-top: 1rem;
+		display: flex;
+		position: relative;
+		justify-content: center;
+		width: 100%;
+		gap: 5em;
+		align-items: center;
+	}
+
+	a {
+		padding: 1em;
+		text-decoration: none;
+		color: black;
+		transition: all 0.2s ease-in-out;
+		width: 6em;
+		text-align: center;
+		border-radius: 0.25em;
+		font-weight: 800;
+	}
+
+	a:hover {
+		color: white;
+		background-color: black;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		a {
+			color: white;
+		}
+
+		a:hover {
+			background-color: white;
+			color: black;
+		}
+	}
+</style>
