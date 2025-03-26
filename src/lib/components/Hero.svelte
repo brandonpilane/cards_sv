e14bceef8ee70536c3deed7de7e9515523d82078
Brandon <BrandonPilane@gmail.com>
2025-03-26 16:43:52 +0200

    feat: add blur pattern background

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -11,6 +11,13 @@
 			UI.
 		</p>
 	</div>
+
+	<div class="bg_pattern">
+		<div class="shape"></div>
+		<div class="shape"></div>
+		<div class="shape"></div>
+		<div class="shape"></div>
+	</div>
 </div>
 
 <style>
@@ -22,6 +29,102 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+	}
+
+	.bg_pattern {
+		z-index: -4;
+		position: absolute;
+		width: 100vw;
+		height: 100vh;
+		top: 0;
+
+		.shape {
+			z-index: inherits;
+			position: absolute;
+			width: 200px;
+			height: 200px;
+			background-color: #9ef01aa4;
+			filter: blur(80px);
+			border-radius: 50%;
+			animation: float 20s infinite alternate ease-in-out;
+		}
+
+		/* Position different shapes randomly */
+		.shape:nth-child(1) {
+			width: 400px;
+			height: 200px;
+			top: 10%;
+			left: 20%;
+			animation-duration: 25s;
+			animation-direction: alternate-reverse;
+		}
+
+		.shape:nth-child(2) {
+			background-color: #38b0008f;
+			width: 300px;
+			height: 300px;
+			bottom: 10%;
+			left: 27%;
+			animation-duration: 30s;
+			animation-delay: 10s;
+		}
+
+		.shape:nth-child(3) {
+			background-color: #33c733a4;
+			top: 56%;
+			left: 66%;
+			animation-duration: 35s;
+			animation-delay: 3s;
+			animation-direction: alternate-reverse;
+		}
+
+		.shape:nth-child(4) {
+			top: 22%;
+			left: 80%;
+			animation-duration: 40s;
+		}
+	}
+
+	/* .bg_pattern::before {
+			content: '';
+			position: absolute;
+			width: 100vw;
+			height: 80vh;
+			top: 0;
+			left: 0;
+			z-index: 10;
+			border-radius: 3rem;
+
+			box-shadow:
+				/* inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010, 
+				inset 0 0 1rem #101010,
+				inset 0 0 1rem #101010;
+		} */
+
+	/* Animation for smooth floating */
+	@keyframes float {
+		0% {
+			transform: translate(0, 0) scale(1); /* Start at the origin */
+		}
+		25% {
+			transform: translate(10rem, -16rem) scale(1.1); /* Move to the right and up */
+		}
+		50% {
+			transform: translate(30rem, -3em) scale(1); /* Move to the far right */
+		}
+		75% {
+			transform: translate(9.2rem, 12rem) scale(0.9); /* Move down and right */
+		}
+		100% {
+			transform: translate(2rem, 17rem) scale(1); /* Return to the origin */
+		}
 	}
 
 	.cards-container {
